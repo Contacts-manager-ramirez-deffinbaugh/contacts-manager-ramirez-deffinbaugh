545e729c6e37ab231630faa9d7d67b3c4833dc61
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-06-02 12:47:02 -0600

    added input validation

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,19 @@ import static java.nio.file.Files.readAllLines;
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static ArrayList<Contact> contacts = new ArrayList<>();
+
     public static void main(String[] args) {
-
-
-        //this creates the contact list from the file
+        //create the contact list from the file
         try {
             getContactsFromList();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+        //call menu
         menu();
 
     }
-
-
 
     public static void menu() {
 
@@ -40,78 +38,106 @@ public class Main {
                 + "Enter an option (1, 2, 3, 4 or 5):";
         System.out.println(options);
 
-        int userOption = scanner.nextInt();
-        scanner.nextLine();
+        int userOption = getInt(1, 5);
         userSelection(userOption);
 
     }
 
     public static void userSelection(int x) {
-            if (x == 1) {
-                view();
-            } else if (x == 2) {
-                add();
-            } else if (x == 3) {
-                search();
-            } else if (x == 4) {
-                delete();
-            } else if (x == 5) {
-                exit();
-            } else {
-                System.out.println("Invalid Entry\n");
-                menu();
-            }
+        if (x == 1) {
+            view();
+        } else if (x == 2) {
+            add();
+        } else if (x == 3) {
+            search();
+        } else if (x == 4) {
+            delete();
+        } else if (x == 5) {
+            exit();
+        } else {
+            System.out.println("Something went wrong.\n");
+            menu();
+        }
     }
-
 
     public static void view() {
 
-        String listHeader = "Name | Phone number\n"
-                        + "--------------------";
+        String listHeader = "Name                |  Phone number\n"
+                + "----------------------------------------";
         System.out.println(listHeader);
         for (Contact contact : contacts) {
-                contact.displayContacts();
+            contact.displayContacts();
         }
         System.out.println(" ");
         menu();
     }
 
-
-    public static void  add() {
-        System.out.println("What is the name of your contact?");
+    public static void add() {
+        System.out.println("What is the name of the contact you would like to add?");
         String nameInput = scanner.nextLine();
-        contactExists(nameInput);
+        boolean contactExists = contactExists(nameInput);
 
-        System.out.println("What is their phone number?");
-        //creat a number function
-        int numberInput = Integer.parseInt(scanner.nextLine());
+        if (!contactExists) {
+            System.out.println("What is their phone number?");
+            //creat a number function
+            long numberInput = getPhoneNumber();
 
-        contacts.add(new Contact(nameInput, numberInput));
+            contacts.add(new Contact(nameInput, numberInput));
+
+            System.out.println("Contact \"" + nameInput + "\" created successfully\n");
+        }
 
         menu();
     }
 
-    public static void contactExists(String nameInput) {
-        for(Contact contact : contacts) {
-            if(contact.getName().equals(nameInput)) {
-                System.out.println("\nThere is already a contact named " + nameInput +".");
-                System.out.println("Would you like to overwrite it? (Yes/No)");
-                menu();
+    public static long getPhoneNumber() {
+        long userIn = getLong();
+        while (String.valueOf(userIn).length() != 10 && String.valueOf(userIn).length() != 7) {
+            System.out.println("Please enter a valid 7 or 10 digit phone number.");
+
+            userIn = getPhoneNumber();
+        }
+        return userIn;
+    }
+
+    public static boolean contactExists(String nameInput) {
+        boolean exists = false;
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getName().equals(nameInput)) {
+                exists = true;
+                System.out.println("\nThere is already a contact named " + nameInput + ".");
+                System.out.println("Would you like to overwrite it? (Type \"yes\" to continue)");
+                String userIn = scanner.nextLine();
+                if (userIn.equalsIgnoreCase("yes")) {
+                    System.out.println("What is their phone number?");
+                    //creat a number function
+                    long numberInput = getPhoneNumber();
+                    contacts.set(i, new Contact(nameInput, numberInput));
+                    System.out.println("Contact \"" + nameInput + "\" updated successfully\n");
+                } else {
+                    System.out.println("Contact overwrite canceled.\n");
+                }
             }
         }
+        return exists;
     }
 
     public static void search() {
         System.out.println("Enter the name of the contact you are looking for: ");
         String contactName = scanner.nextLine();
         boolean found = false;
-        for(Contact contact : contacts) {
-            if( contact.getName().equals(contactName)) {
+        for (Contact contact : contacts) {
+            if (contact.getName().equals(contactName)) {
+                System.out.println("Result:");
+                String listHeader = "Name                |  Phone number\n"
+                        + "----------------------------------------";
+                System.out.println(listHeader);
                 contact.displayContacts();
+                System.out.println();
                 found = true;
             }
         }
-        if(!found) {
+        if (!found) {
             System.out.println("Sorry, there are no contacts by that name.");
         }
         menu();
@@ -121,15 +147,15 @@ public class Main {
         System.out.println("Enter the name of the contact you would like to delete: ");
         String contactName = scanner.nextLine();
         boolean found = false;
-        for(int i = 0; i < contacts.size(); i++) {
-            if( contacts.get(i).getName().equals(contactName)) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getName().equals(contactName)) {
                 contacts.remove(contacts.get(i));
-                System.out.println("Contact \""+ contactName + "\" was deleted successfully");
+                System.out.println("Contact \"" + contactName + "\" was deleted successfully\n");
                 found = true;
             }
         }
-        if(!found) {
-            System.out.println("Sorry, there are no contacts by that name.");
+        if (!found) {
+            System.out.println("Sorry, there are no contacts by that name.\n");
         }
         menu();
     }
@@ -141,6 +167,37 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Contacts were unable to save. You now have no contacts. Wha Wha Wha");
         }
+    }
+
+
+    public static int getInt() {
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (Exception e) {
+            System.out.println("That was not an integer. Try again:");
+            return getInt();
+        }
+    }
+
+    public static int getInt(int min, int max) {
+        int userIn = getInt();
+        while (userIn < min || userIn > max) {
+            System.out.println("That wasn't an option, try again:");
+
+            userIn = getInt();
+        }
+        return userIn;
+    }
+
+    public static long getLong() {
+        long userNumber;
+        try {
+            userNumber = Long.parseLong(scanner.nextLine());
+        } catch (Exception e) {
+            System.out.println("Please enter valid numbers:");
+            userNumber = getLong();
+        }
+        return userNumber;
     }
 
     private static List<String> getFileStringsFromContacts(List<Contact> contacts) {
@@ -170,7 +227,7 @@ public class Main {
 
     public static void getContactsFromList() throws IOException {
         List<String> contactList = getFileList();
-        for(String contact : contactList) {
+        for (String contact : contactList) {
             String[] contactSplit = contact.split(",");
             //TODO wrap conversion
             long number = Long.parseLong(contactSplit[1]);
